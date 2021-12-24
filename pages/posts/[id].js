fa97ca26a1f5008552a6a99cb17f1441eb1204e2
p-acid <qkrtkstjd01@gmail.com>
2021-12-24 14:23:20 +0900

    modify: finalize practice

---
+++ pages/posts/[id].js
@@ -0,0 +1,40 @@
+import Head from "next/head";
+import Layout from "../../components/Layout";
+import Date from "../../components/date";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+
+export default function Post({ postData }) {
+  return (
+    <>
+      <Head>
+        <title>All Posts | {postData.title}</title>
+      </Head>
+      <Date dateString={postData.date} />
+      <Layout>
+        {postData.title}
+        <br />
+        {postData.id}
+        <br />
+        {postData.date}
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </Layout>
+    </>
+  );
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
