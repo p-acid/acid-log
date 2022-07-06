77eb72a1d56caeb0d1206e28af9f30c64493e6b7
p-acid <qkrtkstjd01@gmail.com>
2022-07-06 20:12:14 +0900

    Modify: anchor

--- components/common/Link/AnchorStyle.ts
+++ components/common/Link/AnchorStyle.ts
@@ -3,6 +3,8 @@ import styled from "@emotion/styled";
 export const StyledAnchor = styled.a`
   padding: ${({ theme }) => (theme.figure / 2) * 3}px
     ${({ theme }) => (theme.figure / 2) * 3}px;
+  text-decoration: none;
+  color: ${({ theme }) => theme.colors.black_80};
 
   ${({ theme }) => theme.typography.text_md};
 `;
