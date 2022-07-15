278bb24436960874ab6710d962467fcf24be296a
p-acid <qkrtkstjd01@gmail.com>
2022-07-15 23:21:03 +0900

    Modify: design

--- container/pages/PostMain/PostMain.tsx
+++ container/pages/PostMain/PostMain.tsx
@@ -17,20 +17,25 @@ import { getEachDate } from "../../../utils/date";
 
 const PostMain = ({ postData }: PostMainProps) => {
   const { query } = useRouter();
-  const { title, thumbnail, date, description } = postData;
+  const { title, thumbnail, date, description, tocHtml } = postData;
 
   const [year, month, day] = getEachDate(new Date(date));
 
   return (
-    <PostThumbnailWrapper>
-      <PostThumbnailMask />
-      <PostThumbnail src={`${URL.IMAGE.POSTS}/${query.postId}/${thumbnail}`} />
-      <PostInfoWrapper>
-        <PostTitle>{title}</PostTitle>
-        <PostDescription>{description}</PostDescription>
-        <PostDate>{`${year}년 ${month}월 ${day}일`}</PostDate>
-      </PostInfoWrapper>
-    </PostThumbnailWrapper>
+    <div>
+      <PostThumbnailWrapper>
+        <PostThumbnailMask />
+        <PostThumbnail
+          src={`${URL.IMAGE.POSTS}/${query.postId}/${thumbnail}`}
+        />
+        <PostInfoWrapper>
+          <PostTitle>{title}</PostTitle>
+          <PostDescription>{description}</PostDescription>
+          <PostDate>{`${year}년 ${month}월 ${day}일`}</PostDate>
+        </PostInfoWrapper>
+      </PostThumbnailWrapper>
+      <div>{tocHtml}</div>
+    </div>
   );
 };
 
--- pages/post/[postId].tsx
+++ pages/post/[postId].tsx
@@ -1,9 +1,19 @@
+import Head from "next/head";
+
 import PostMain from "../../container/pages/PostMain/PostMain";
 import { getPostData } from "../../utils/post";
 import { getAllPostPaths } from "../../utils/post";
 
 const Post = (props: any) => {
-  return <PostMain {...props} />;
+  return (
+    <>
+      <Head>
+        <title>Acidlog | {props.postData.title}</title>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <PostMain {...props} />
+    </>
+  );
 };
 
 export default Post;
