91066dc2a5810034d621302fcca6732778fab178
p-acid <qkrtkstjd01@gmail.com>
2022-07-16 22:31:34 +0900

    Remove: console

--- container/pages/LogMain/LogMain.tsx
+++ container/pages/LogMain/LogMain.tsx
@@ -9,8 +9,6 @@ import {
 import { LogMainProps } from "../../containerType";
 
 const LogMain = ({ allLogs }: LogMainProps) => {
-  console.log(allLogs);
-
   return (
     <LogMainWrapper>
       <LogMainDescription>말 그대로 짧은 기록들을 담습니다</LogMainDescription>
